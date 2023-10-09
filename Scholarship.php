b2dcaea215b2d64f37987f7d2b2a5533f1ddafa7
Ayboss1 <139301677+Ayboss1@users.noreply.github.com>
2023-10-09 10:14:20 +0100

    Create Scholarship.php

---
+++ Scholarship.php
@@ -0,0 +1,9 @@
+<html>
+ <body>
+    Congratulations<?php echo $_post["fname"]; ?>
+    <br>Your Scholarship application has been successfully received and will be processed shortly.<br>
+    Details about the date for the exam will be gotten accross to you on <?php echo $_post["mail"]?> and <?php echo $_post["phoneno"]?>
+    
+ </body>
+    
+</html>
